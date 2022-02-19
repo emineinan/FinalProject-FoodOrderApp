b86e8175313775bca63ed48f285a4abdd8f971be
Emine İnan <emineinanx@gmail.com>
2022-02-19 21:57:35 +0300

    Add basket menu

--- app/src/main/java/com/example/foodorderapp/fragments/FoodListFragment.kt
+++ app/src/main/java/com/example/foodorderapp/fragments/FoodListFragment.kt
@@ -1,15 +1,16 @@
 package com.example.foodorderapp.fragments
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.example.foodorderapp.R
 import com.example.foodorderapp.adapter.foodadapter.FoodListAdapter
 import com.example.foodorderapp.databinding.FragmentFoodListBinding
+import com.example.foodorderapp.model.food.Food
 import com.example.foodorderapp.viewmodel.FoodListViewModel
 
 class FoodListFragment : Fragment() {
@@ -24,7 +25,8 @@ class FoodListFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_food_list, container, false)
 
         binding.foodListFragment = this
-        binding.foodListToolbar = "Foods"
+        binding.foodListToolbar = "Anasayfa"
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarFoodList)
 
         viewModel.foodList.observe(viewLifecycleOwner, {
             foodListAdapter = FoodListAdapter()
@@ -36,8 +38,23 @@ class FoodListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
 
         val tempViewModel: FoodListViewModel by viewModels()
         viewModel = tempViewModel
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.basket_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_basket) {
+            val action =
+                FoodListFragmentDirections.actionFoodListFragmentToFoodBasketFragment()
+            Navigation.findNavController(binding.root).navigate(action)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
